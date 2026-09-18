9e06d8555cc107d1b0f312aee17de4e0e9022ba2
Kyle Aure <KyleJAure@gmail.com>
2026-09-18 16:31:42 -0500

    feat: add a challenged annotation for automatic documentation

---
+++ tck/src/main/java/ee/jakarta/tck/json/bind/framework/junit/anno/Challenge.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package ee.jakarta.tck.json.bind.framework.junit.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.Disabled;
+
+/**
+ * Challenge metadata to track where challenges from GitHub came from,
+ * what version was affected by the challenge,
+ * and if the issue has been fixed.
+ *
+ * Aggregates the @Disabled annotation from Junit
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Disabled
+public @interface Challenge {
+    /**
+     * REQUIRED: Link to the challenge on GitHub
+     */
+    String link();
+    
+    /**
+     * REQUIRED: The version of the TCK where the challenge was raised
+     */
+    String version();
+    
+    /**
+     * OPTIONAL: The reason the TCK was disabled
+     */
+    String reason() default "";
+}
